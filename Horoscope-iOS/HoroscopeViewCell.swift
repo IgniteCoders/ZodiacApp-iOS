e4a6ca1f25981340568836b83ffa4f512d6199bd
IgniteCoders <theboom.kajas@gmail.com>
2025-11-06 12:33:39 +0100

    We prepared the data and the TableView

---
+++ Horoscope-iOS/HoroscopeViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  HoroscopeViewCell.swift
+//  Horoscope-iOS
+//
+//  Created by Mananas on 6/11/25.
+//
+
+import UIKit
+
+class HoroscopeViewCell: UITableViewCell {
+
+    @IBOutlet weak var signImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var datesLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func render(_ horoscope: Horoscope) {
+        nameLabel.text = horoscope.name
+        datesLabel.text = horoscope.dates
+        signImageView.image = horoscope.getSignIcon()
+    }
+
+}
--- Horoscope-iOS/ViewController.swift
+++ Horoscope-iOS/ViewController.swift
@@ -7,11 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let horoscopeList: [Horoscope] = Horoscope.getAll()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return horoscopeList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HoroscopeCell", for: indexPath) as! HoroscopeViewCell
+        let horoscope = horoscopeList[indexPath.row]
+        cell.render(horoscope)
+        return cell
     }
 
 
